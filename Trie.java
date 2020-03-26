6c759397f9c245a6c212723081066ade4b9ab3d9
Jonecon <connor.gauthern@gmail.com>
2020-03-26 15:03:04 +1300

    Refactored the trie to work with a linked list instead of an array

--- Trie.java
+++ Trie.java
@@ -9,25 +9,23 @@ public class Trie{
 	//Setup the global variables.
 	final int SIZE = 256;
 	int nextFree;
-	private TrieNode root;
+	private Node root;
 	
-	//Setup the trieNode.
-	class TrieNode{
-		//Has a value which is the phase num.
+	public class Node{
+		//Has a value which is the phase num, and a byte that it's representing.
 		public int value;
+		public byte contains;
 		
-		//Setup all the possible points to its children.
-		TrieNode[] child = new TrieNode[SIZE];
+		//Can go across, and down levels since it's a trie
+		public Node nextNode;
+		public Node nextLevel;
 		
-		//Constructor which initializes all of the children to null, and assigns the node the next free phrase number.
-		public TrieNode(){
-			//For every child make it null.
-			for (int i = 0; i < SIZE; i++)
-				child[i] = null;
-			
-			//Give it the next phrase number.	
+		//Node constructor.
+		public Node(byte b){
+			contains = b;
+			nextNode = null;
+			nextLevel = null;
 			value = nextFree;
-			//Incriment that number by 1.
 			nextFree++;
 		}
 	}
@@ -35,50 +33,88 @@ public class Trie{
 	//Creates a trie, populating all the children of the root node to every possible input from a byte.
 	public Trie(){
 		//Setting root node phrase num to -1
-		nextFree = -1;
+		nextFree = 0;
 		//Creating root node.
-		root = new TrieNode();
+		root = new Node((byte)0);
+		//Crawler to go cycle through root.
+		Node crawl = root;
+		//TrieNode TrieCrawl = root;
 		//Setup every child of root.
-		for(int i = 0; i < SIZE; i++){
-			root.child[i] = new TrieNode();
+		for(int i = 1; i < SIZE; i++){
+			crawl.nextNode = new Node((byte)i);
+			crawl = crawl.nextNode;
 		}
 	}
 	
 	public void insert(ArrayList<Byte> key){
-		TrieNode curr = root;
+		Node currNode = root;
 		int pos;
+		byte keyCurr;
+		int size = key.size();
 		//For every byte in this list, go down the trie until you find the missmatch, then insert.
-		for(int i = 0; i < key.size(); i++){
-			//Int value of the byte.
-			pos = (int)key.get(i);
+		for (int i = 0; i < size; i++){
+			keyCurr = key.get(i);		
+			//Fuck me in the ass and call me a donkey
 			
-			//Missmatch was found, add this phrase.
-			if(curr.child[pos] == null){
-				curr.child[pos] = new TrieNode();
+			if (size - 1 == i){
+				while (currNode.nextNode != null)
+					currNode = currNode.nextNode;
+				currNode.nextNode = new Node(keyCurr);
 				return;
 			}
 			
-			//Not a missmatch go to next level of nodes.	
-			curr = curr.child[pos];
+			while (currNode.nextNode != null){
+				int res = Byte.compare(keyCurr, currNode.contains);
+				if (res == 0 && currNode.nextLevel == null){
+					currNode.nextLevel = new Node(key.get(key.size() - 1));
+					return;
+				}
+				
+				if (res == 0){
+					currNode = currNode.nextLevel;
+					break;
+				}
+				
+				currNode = currNode.nextNode;
+			}
 		}
 	}
 	
 	public int find(ArrayList<Byte> key){
-		TrieNode curr = root;
-		int pos;
-		for(int i = 0; i < key.size(); i++){
-			//Int value of byte.
-			pos = (int)key.get(i);
+		Node currNode = root;
+		byte keyCurr;
+		int size = key.size();
+		for(int i = 0; i < size; i++){
+			keyCurr = key.get(i);
+			//Shit cunt mother fucker.
 			
-			//The pattern is not in the trie return -1
-			if(curr.child[pos] == null)
+			currNode = searchLevel(keyCurr, currNode);
+			
+			if (currNode == null)
+				return -1;
+				
+			if (size - 1 == i)
+				return currNode.value;
+				
+			if (currNode.nextLevel == null)
 				return -1;
 			
-			//Pattern is in the trie return the phrase number.	
-			curr = curr.child[pos];
+			currNode = currNode.nextLevel;
+			
+		}
+		return -1;	
+	}
+	
+	public Node searchLevel(byte key, Node curr){
+		
+		while (curr.nextNode != null){
+			int res = Byte.compare(key, curr.contains);
+			if (res == 0)
+				return curr;
+			curr = curr.nextNode;
 		}
 	
-	
-		return curr.value;	
+		return null;
 	}
+	
 }
