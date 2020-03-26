a7c50fd8a1ed17d92c8d1c467cc56c4fd512e1ed
Mason Elliott <mason.elliott12@gmail.com>
2020-03-26 06:20:14 +0100

    Adding packer

---
+++ LZWpack.java
@@ -0,0 +1,69 @@
+import java.io.*;
+
+class LZWpack{
+
+    public static void main (String[] args)
+	{
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+            int initialPhraseLength = 128;
+            int phraseLength = 255;
+            int bitsNeededForPhrase = 8;
+            int packingBit = 0;
+            int inputBit;
+            int outputMask = 0xFF000000;
+            int bitsInPacker = 0;
+            int startOfBitInput = 32;
+            int initailShiftBits = 0;
+            int output;
+            byte outputByte;
+            String line;  
+  
+
+            
+            while((line = in.readLine()) != null){  
+                inputBit = Integer.parseInt(line);
+                //inputBit = 255;
+                initailShiftBits = (startOfBitInput - bitsInPacker) - bitsNeededForPhrase;
+                inputBit = inputBit << initailShiftBits;
+
+
+                //Adding it to the packing bit
+                packingBit = packingBit | inputBit;
+
+                bitsInPacker = bitsInPacker + bitsNeededForPhrase;
+
+                //Outputing bits
+                while(bitsInPacker >= 8){
+                    //Output
+                    output = packingBit & outputMask;
+                    //System.out.println(output);
+                    output = output >> 24;
+                    outputByte = (byte)output;
+                    //System.out.println(outputByte);
+                    output = 0;
+                    //Adjusting bits in packer
+                    bitsInPacker = bitsInPacker - 8; 
+                    packingBit = packingBit << 8;
+                    System.out.write(bitsInPacker);
+                }
+ 
+                if((phraseLength + 1) == (initialPhraseLength + initialPhraseLength)){
+                    bitsNeededForPhrase++;
+                    initialPhraseLength = ((phraseLength + 1) * 2);
+                }
+                phraseLength++;
+                
+            }           
+
+            in.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+        
+    }
+
+}
